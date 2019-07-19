d20d5961354fc7efc04e7ebc3acdc65ae2e18d79
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2019-07-19 13:37:19 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # bsd-node
-NodeJS Library for interacting with the BSD Tools
+
+NodeJS Library for interacting with the BSD Tools.
+
+## Setup
+
+```sh
+$ npm install @ewarren/bsd-node
+```
+
+## Usage
+
+**Convert a two-letter code to the name of a state**
+
+```js
+const bsd = require('@ewarren/bsd-node');
+
+const bsdClient = bsd('client.bsd.net', 'your_app_id', 'your_app_secret');
+
+const response = await client.get('/page/api/signup/list_form_fields', {
+  'signup_form_id': '123',
+});
+
+console.log(response.statusCode);
+console.log(response.data.api.signup_form_field);
+```
+
+## Local development
+
+Coming soon...
---
+++ src/index.js
@@ -0,0 +1,103 @@
+const crypto = require('crypto');
+const request = require('request-promise-native');
+const parser = require('fast-xml-parser');
+
+const API_VERSION_KEY = 'api_ver';
+const API_APP_ID_KEY = 'api_id';
+const API_TIMESTAMP = 'api_ts';
+const API_MAC = 'api_mac';
+
+module.exports = function init(clientUrl = null, appId = null, appSecret = null, apiVersion = '2') {
+  if (! clientUrl || ! appId || ! appSecret) {
+    console.error('WARNING: Missing configuration details for bsd-node');
+  }
+
+  const apiTimestamp = Math.floor(Date.now() / 1000);
+
+  const defaultParameters = {
+    [API_APP_ID_KEY]: appId,
+    [API_TIMESTAMP]: apiTimestamp,
+    [API_VERSION_KEY]: apiVersion,
+  };
+
+  function joinParameters(parameters, encode = true) {
+    const parts = Object.keys(parameters).reduce((acc, key) => {
+      const value = parameters[key];
+
+      if (encode) {
+        return [
+          ...acc,
+          [encodeURIComponent(key), encodeURIComponent(value)],
+        ];
+      }
+
+      return [
+        ...acc,
+        [key, value],
+      ];
+    }, []);
+
+    const stringified = parts.map(([key, value]) => `${key}=${value}`);
+
+    return stringified.join('&');
+  }
+
+  async function makeApiMac(path, parameters) {
+    const finalParameters = joinParameters({
+      ...defaultParameters,
+      ...parameters,
+    }, false);
+
+    const signingString = `${appId}\n${apiTimestamp}\n${path}\n${finalParameters}`;
+    console.log(signingString);
+
+    const hmac = crypto.createHmac('sha1', appSecret);
+    hmac.setEncoding('hex');
+
+    return new Promise((resolve) => {
+      hmac.end(signingString, () => resolve(hmac.read()));
+    });
+  }
+
+  async function makeRequest(path, options = {}) {
+    try {
+      const response = await request(path, {
+        resolveWithFullResponse: true,
+        ...options,
+      });
+
+      const data = parser.parse(response.body || '');
+      
+      return {
+        ...response,
+        data,
+      };
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
+
+  async function get(path, parameters) {
+    try {
+      const apiMac = await makeApiMac(path, parameters);
+
+      const finalParameters = joinParameters({
+        ...defaultParameters,
+        [API_MAC]: apiMac,
+        ...parameters,
+      });
+
+      const finalPath = `${clientUrl}${path}?${finalParameters}`;
+
+      return await makeRequest(finalPath);
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
+
+  return {
+    get,
+  };
+}
